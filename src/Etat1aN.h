deeb09951ac1a3bf5044af5d3549b7c6aaa71411
camille pernoud <camille.pernoud.2001@gmail.com>
2015-03-08 10:59:28 +0100

    Ajout des états

---
+++ src/Etat1aN.h
@@ -0,0 +1,410 @@
+#ifndef ETAT1AN_H
+#define ETAT1AN_H
+
+
+class Etat0
+{
+	public:
+		Etat0();
+		virtual ~Etat0();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat1
+{
+	public:
+		Etat1();
+		virtual ~Etat1();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat2
+{
+	public:
+		Etat2();
+		virtual ~Etat2();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat3
+{
+	public:
+		Etat3();
+		virtual ~Etat3();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat4
+{
+	public:
+		Etat4();
+		virtual ~Etat4();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat5
+{
+	public:
+		Etat5();
+		virtual ~Etat5();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat6
+{
+	public:
+		Etat6();
+		virtual ~Etat6();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat7
+{
+	public:
+		Etat7();
+		virtual ~Etat7();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat8
+{
+	public:
+		Etat8();
+		virtual ~Etat8();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat9
+{
+	public:
+		Etat9();
+		virtual ~Etat9();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat10
+{
+	public:
+		Etat10();
+		virtual ~Etat10();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat11
+{
+	public:
+		Etat11();
+		virtual ~Etat11();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat12
+{
+	public:
+		Etat12();
+		virtual ~Etat12();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat13
+{
+	public:
+		Etat13();
+		virtual ~Etat13();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat14
+{
+	public:
+		Etat14();
+		virtual ~Etat14();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat15
+{
+	public:
+		Etat15();
+		virtual ~Etat15();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat16
+{
+	public:
+		Etat16();
+		virtual ~Etat16();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat17
+{
+	public:
+		Etat17();
+		virtual ~Etat17();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat18
+{
+	public:
+		Etat18();
+		virtual ~Etat18();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat19
+{
+	public:
+		Etat19();
+		virtual ~Etat19();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat20
+{
+	public:
+		Etat20();
+		virtual ~Etat20();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat21
+{
+	public:
+		Etat21();
+		virtual ~Etat21();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat22
+{
+	public:
+		Etat22();
+		virtual ~Etat22();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat23
+{
+	public:
+		Etat23();
+		virtual ~Etat23();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat24
+{
+	public:
+		Etat24();
+		virtual ~Etat24();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat25
+{
+	public:
+		Etat25();
+		virtual ~Etat25();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat26
+{
+	public:
+		Etat26();
+		virtual ~Etat26();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat27
+{
+	public:
+		Etat27();
+		virtual ~Etat27();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat28
+{
+	public:
+		Etat28();
+		virtual ~Etat28();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat29
+{
+	public:
+		Etat29();
+		virtual ~Etat29();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat30
+{
+	public:
+		Etat30();
+		virtual ~Etat30();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat31
+{
+	public:
+		Etat31();
+		virtual ~Etat31();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat32
+{
+	public:
+		Etat32();
+		virtual ~Etat32();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat33
+{
+	public:
+		Etat33();
+		virtual ~Etat33();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat34
+{
+	public:
+		Etat34();
+		virtual ~Etat34();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat35
+{
+	public:
+		Etat35();
+		virtual ~Etat35();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat36
+{
+	public:
+		Etat36();
+		virtual ~Etat36();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat37
+{
+	public:
+		Etat37();
+		virtual ~Etat37();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat38
+{
+	public:
+		Etat38();
+		virtual ~Etat38();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat39
+{
+	public:
+		Etat39();
+		virtual ~Etat39();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat40
+{
+	public:
+		Etat40();
+		virtual ~Etat40();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat41
+{
+	public:
+		Etat41();
+		virtual ~Etat41();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat42
+{
+	public:
+		Etat42();
+		virtual ~Etat42();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat43
+{
+	public:
+		Etat43();
+		virtual ~Etat43();
+	protected:
+	private:
+};
+//-------------------------------------------
+class Etat44
+{
+	public:
+		Etat44();
+		virtual ~Etat44();
+	protected:
+	private:
+};
+//-------------------------------------------
+#endif // ETAT1AN_H
