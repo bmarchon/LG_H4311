273ded4a9afde19ab04f9ffa38519bfb155f20d4
Anthony FARAUT <anthony.faraut@gmail.com>
2015-03-13 09:43:23 +0100

    reader

--- src/Reader.h
+++ src/Reader.h
@@ -1,14 +1,22 @@
 #ifndef READER_H
 #define READER_H
 
+#include <string>
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+
+using namespace std;
 
 class Reader
 {
 	public:
 		Reader();
 		virtual ~Reader();
-	protected:
+		void lire(string filename);
+		char next();
+		char shift();
 	private:
+		string filetext;
 };
 
 #endif // READER_H
