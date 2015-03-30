208cb0bf9c54a1738e1f9d225731693dff08ba4d
mgarchery <mathieu.garchery@insa-lyon.fr>
2015-03-30 13:47:10 +0200

    transformation du programme par élimination des neutres OK

--- src/Transformation.cpp
+++ src/Transformation.cpp
@@ -17,12 +17,7 @@ Programme * Transformation::getProgramme()
 }
 
 void Transformation::transformer()
-{ /*
-	cout << endl;
-	cout <<"transformation--------------------------------------------" << endl;
-	//Programme * leProgramme=getProgramme();
-	//leProgramme.afficher();
-	*/
+{ 
 	vector<Instruction*> instructions = this->programme->getInstructions();
 	for(auto it = instructions.begin(); it != instructions.end(); ++it)
 	{
@@ -40,31 +35,7 @@ void Transformation::transformer()
 		}
 	}
 
-
-	/*
-	// trace : cout << instructions.size()<< endl;
-	for(unsigned int i=0; i<instructions.size(); i++) {
-		instructions[i]->afficher();
-		if (instructions[i]-> getInstType() == ECR)
-		{
-			InstructionEcriture* instru = (InstructionEcriture*)instructions[i];
-			Expression * lExpressionMere = instru->getExpression();
-			//cout <<"ecriture"<<endl;
-			anaExpr(lExpressionMere, lExpressionMere);
-		}
-		if (instructions[i]-> getInstType() == AFF)
-		{
-			InstructionAffectation* instru = (InstructionAffectation*)instructions[i];
-			Expression * lExpressionMere = instru->getExpression();
-			//cout<<"affect"<<endl;
-			anaExpr(lExpressionMere, lExpressionMere);
-		}
-	}
-	cout <<"fin de la transformation-----------------------------------------"<< endl;
-	for(unsigned int i=0; i<instructions.size(); i++) {
-		instructions[i]->afficher();
-	}*/
-	}
+}
 
 //Fonction appelée récursivement
 Expression * Transformation::simplifier(Expression * exp)
@@ -175,76 +146,14 @@ Expression * Transformation::simplifier(Expression * exp)
 	return res;
 }
 
-	bool Transformation::is0Const(Expression * exp)
-	{
-		return ((exp->getExprType() == VALEUR) && (exp->eval() == 0.0));
-	}
+bool Transformation::is0Const(Expression * exp)
+{
+	return ((exp->getExprType() == VALEUR) && (exp->eval() == 0.0));
+}
 
-	bool Transformation::is1Const(Expression * exp)
-	{
-		return ((exp->getExprType() == VALEUR) && (exp->eval() == 1.0));
-	}
+bool Transformation::is1Const(Expression * exp)
+{
+	return ((exp->getExprType() == VALEUR) && (exp->eval() == 1.0));
+}
 
-/*
-	//cout <<"dans anaExpr"<<endl;
-	Expressions leType = lExpressionFille->getExprType();
-	if (leType == BIN)
-	{
-		cout << "expression bin"<<endl;
-		ExprBinaire* lExpressionFilleCastee = (ExprBinaire *)lExpressionFille;
-		Expression * lExpressionFilleG = lExpressionFilleCastee->getGauche();
-		Expression * lExpressionFilleD = lExpressionFilleCastee->getDroite();
-		if (lExpressionFilleCastee->eval() == lExpressionFilleG->eval())
-		{
-			cout <<"expression gauche egale"<<endl;
-			//pb : if (lExpressionMere->getExpression()->getExprType() == BIN) cout << "bin"<<endl;
-			cout <<"lExpressionMere : "<<lExpressionMere<<endl;
-			cout <<"lExpressionMere->getExpression() : "<<lExpressionMere->getExpression()<<endl;
-			//if (lExpressionMere->getExprType() == BIN) cout << "bin"<<endl;
-			//double valeur = lExpressionFilleCastee->eval();
-			//cout <<"Valeur : "<<valeur<<endl;
-			lExpressionMere->setExpression(lExpressionFilleG);
-			cout <<"lExpressionMere->getExpression() : "<<lExpressionMere->getExpression()<<endl; 
-			//valeur = lExpressionMere->getExpression()->eval();
-			//cout <<"Valeur : "<<valeur<<endl;
-		}
-		else if (lExpressionFilleCastee->eval() == lExpressionFilleD->eval())
-		{
-			cout <<"expression droite egale"<<endl;
-			cout <<"lExpressionMere : "<<lExpressionMere<<endl;
-			cout <<"lExpressionMere->getExpression() : "<<lExpressionMere->getExpression()<<endl;
-			//if (lExpressionMere->getExprType() == BIN) cout << "bin"<<endl;
-			//double valeur = lExpressionFilleCastee->eval();
-			//cout <<"Valeur : "<<valeur<<endl;
-			lExpressionMere->setExpression(lExpressionFilleD);
-			// trace : cout <<"lExpressionMere->getExpression() : "<<lExpressionMere->getExpression()<<endl; 
-			//valeur = lExpressionFilleD->getExpression()->eval();
-			//cout <<"Valeur : "<<valeur<<endl; 
-		}
-		else
-		{
-			cout << "gauche"<<endl;
-			anaExpr(lExpressionFilleG, lExpressionFille);
-			cout << "droite"<<endl;
-			anaExpr(lExpressionFilleD, lExpressionFille);
-		}
-		
-	}
-	if (leType == PAR)
-	{
-		cout << "expression paranthesee"<<endl;
-		ExprPar* lExpressionFilleCastee = (ExprPar *)lExpressionFille;
-		Expression * nvlExpressionMere = lExpressionFilleCastee->getExpression();
-		anaExpr(nvlExpressionMere, lExpressionFille);
-	}
-	if (leType == VALEUR)
-	{
-		cout << "valeur"<< " : " <<lExpressionFille->eval()<<endl;
-
-	}
-	if (leType == IDENT)
-	{
-		cout << "identifiant"<<endl;
-	}
-*/
 
